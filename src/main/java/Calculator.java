e9010e2cc3ab7535e36aa8bd8a5920d55632676a
IrinaMazur <irinka.mazur.04@gmail.com>
2024-11-19 21:29:31 +0300

    добавлен метод проверки корректности выражения

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,6 @@
+/**
+ * A class that evaluates the value of an expression.
+ */
 public class Calculator {
     /**
      * Expression containing numbers, operators, brackets, spaces, other characters.
@@ -23,4 +26,85 @@ public class Calculator {
                 NewExpression.append(Expression.charAt(index));
         Expression = NewExpression.toString();
     }
+
+    /**
+     * The method of checking the expression for correctness
+     * @return True if the expression is true, otherwise false
+     */
+    private boolean Correctness()
+    {
+        if (Expression.isEmpty()) return false;
+        else {
+            DeleteSpace();
+            int bracket = 0;
+
+            for (int index = 0; index < Expression.length(); index++) {
+
+                if (bracket >= 0) {
+
+                    switch (Expression.charAt(index)) {
+
+                        case '+': case '-': case '*': case '/': {
+                            if(index == 0 || index == Expression.length() - 1) return false;
+                            else
+                            if (Expression.charAt(index+1) == '+' ||
+                                    Expression.charAt(index+1) == '-' ||
+                                    Expression.charAt(index+1) == '*' ||
+                                    Expression.charAt(index+1) == '/' ||
+                                    Expression.charAt(index+1) == ')')
+                                return false;
+                            break;
+                        }
+
+                        case '(': {
+                            bracket++;
+                            if (Expression.charAt(index+1) == '+' ||
+                                    Expression.charAt(index+1) == '*' ||
+                                    Expression.charAt(index+1) == '/' ||
+                                    Expression.charAt(index+1) == ')')
+                                return false;
+                            else if (index == Expression.length() - 1) return false;
+                            break;
+                        }
+
+                        case ')': {
+                            bracket--;
+                            if (index == 0) return false;
+                            else if (Expression.charAt(index-1) == '+' ||
+                                    Expression.charAt(index-1) == '-' ||
+                                    Expression.charAt(index-1) == '*' ||
+                                    Expression.charAt(index-1) == '/' ||
+                                    Expression.charAt(index-1) == '(' )
+                                return false;
+                            break;
+                        }
+
+                        case '.': {
+                            if(index == Expression.length()-1) return false;
+                            else if (Expression.charAt(index+1) < '0' ||
+                                    Expression.charAt(index+1) > '9')
+                                return false;
+                            break;
+                        }
+
+                        default:
+                            if (Expression.charAt(index) >= '0' &&
+                                    Expression.charAt(index) <= '9') {
+                                if (index != 0)
+                                    if (Expression.charAt(index-1) == ')' )
+                                        return false;
+                                if (index != Expression.length()-1)
+                                    if (Expression.charAt(index+1) == '(' )
+                                        return false;
+                            }
+                            else return false;
+                    }
+                }
+                else return false;
+            }
+            return bracket == 0;
+        }
+    }
+
+
 }
